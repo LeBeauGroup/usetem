ecf18c2349091ed2b0279fad768a8b6c7b0e46f1
Abinash <akumar25@ncsu.edu>
2019-04-09 19:59:50 -0400

    rate calculation

---
+++ modules/syringePump/pumpCal.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+
+def pumpCal(par,ratioReq,Rate,time):
+
+    concFe = par['Fe']
+
+    concOH = par['OH']
+
+    pH = par['initialpH']
+
+    concH = 10**(-pH)
+
+    volRatio = ((ratioReq*concFe)+concH)/concOH
+
+    factor = 1/(1+volRatio)
+
+    rateFe = factor*Rate
+
+    rateOH = (1-factor)*Rate
+
+    finalpH = 14-(-np.log10(volRatio*concFe*ratioReq-(3*concFe*rateFe*time)-(concH*rateFe*time)))
+
+    return rateFe,rateOH,finalpH
+
+
+par = dict()
+par['Fe'] = 0.3 *10**-3
+
+par['OH'] = 4.53*10**-3
+
+par['initialpH'] = 2.25
+
+
+rateFe,rateOH,finalpH = pumpCal(par,15.2,240,0.5)
+
+rateFe
+
+finalpH
