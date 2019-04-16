e71aaa06ea0832ec4238feebb14b840739183d2c
Abinash <akumar25@ncsu.edu>
2019-04-16 04:08:31 -0400

    GUI is working with pump

--- modules/syringePump/GUI/mainGUI.py
+++ modules/syringePump/GUI/mainGUI.py
@@ -93,17 +93,17 @@ class usePump(QDialog):
 
 
 
-    def pumpRun(self,pumpMain):
+    def pumpRun(self):
 
         if self.checkBoxPumpState0.isChecked() == True:
 
             num =0
 
-            pumpMain.FlowRate(self.lineEditInRate0.text(),self.lineEditInRate0.currentText(),num)
-            pumpMain.Volume(self.lineEditVolumeSet0.text(),self.comboBoxVolumeSetUnit0.currentText(),num)
-            pumpMain.Time(self.lineEditTimeSet0.text(),self.comboBoxTimeSet0.currentText(),num)
+            self.pumpMain.FlowRate(self.lineEditInRate0.text(),self.comboBoxInRate0.currentText(),num)
+            self.pumpMain.Volume(self.lineEditVolumeSet0.text(),self.comboBoxVolumeSetUnit0.currentText(),num)
+            self.pumpMain.Time(self.lineEditTimeSet0.text(),num)
 
-            pumpMain.infuse(num)
+            self.pumpMain.infuse(num)
 
 
 
@@ -112,23 +112,23 @@ class usePump(QDialog):
 
             num=1
 
-            pumpMain.FlowRate(self.lineEditInRate1.text(),self.lineEditInRate1.currentText(),num)
-            pumpMain.Volume(self.lineEditVolumeSet1.text(),self.comboBoxVolumeSetUnit1.currentText(),num)
-            pumpMain.Time(self.lineEditTimeSet1.text(),self.comboBoxTimeSet1.currentText(),num)
-            pumpMain.infuse(num)
+            self.pumpMain.FlowRate(self.lineEditInRate1.text(),self.comboBoxInRate1.currentText(),num)
+            self.pumpMain.Volume(self.lineEditVolumeSet1.text(),self.comboBoxVolumeSetUnit1.currentText(),num)
+            self.pumpMain.Time(self.lineEditTimeSet1.text(),num)
+            self.pumpMain.infuse(num)
 
-    def pumpStop(self,pumpMain):
+    def pumpStop(self):
 
         if self.checkBoxPumpState0.isChecked() == True:
 
             num =0
-            pumpMain.stop(num)
+            self.pumpMain.stop(num)
 
         if self.checkBoxPumpState1.isChecked() == True:
 
             num=1
 
-            pumpMain.stop(num)
+            self.pumpMain.stop(num)
 
 
 
@@ -137,13 +137,13 @@ class usePump(QDialog):
 
         if self.radioButtonLocal.isChecked() == True:
 
-            pumpMain = pumpy.Pump(pumpAdd)
+            self.pumpMain = pumpy.Pump(pumpAdd)
 
             self.labelMessage.setText('Pump is connected Locally')
 
         elif self.radioButtonServer.isChecked() == True:
 
-            pumpMain = pumpConn(self.lineEditIP.text())
+            self.pumpMain = pumpConn(self.lineEditIP.text())
 
             self.labelMessage.setText('Pump is connected through Server')
 
@@ -151,7 +151,7 @@ class usePump(QDialog):
 
             self.labelMessage.setText('No Connection')
 
-        if pumpMain is None:
+        if self.pumpMain is None:
 
             self.labelMessage.setText('Connection Error')
 
@@ -167,3 +167,5 @@ app = QApplication(sys.argv)
 ex = usePump()
 ex.show()
 sys.exit(app.exec())
+
+print(PumpMain)
