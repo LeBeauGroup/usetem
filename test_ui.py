b36d0a61c17c319956b92b4226a44c785a555ff4
subangstrom <jmlebeau@gmail.com>
2019-08-16 14:22:04 -0400

    ui development

---
+++ test_ui.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication
+from test_dialog import Ui_Dialog
+
+class AppWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.show()
+
+
+
+if __name__ == '__main__':
+
+    print('this')
+    app = QApplication(sys.argv)
+    window = AppWindow()
+    window.show()
+    sys.exit(app.exec_())
