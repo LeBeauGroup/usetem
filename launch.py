23b8dd660c3d5ee2d3490a6345766bd95d893099
subangstrom <jmlebeau@gmail.com>
2020-01-24 15:38:46 -0500

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,3 +1,20 @@
+# Universal Scripting Engine for Transmission Electron Microscopy
+# A framework to automate electron microscopy data collection
+# Copyright (C) 2020 James M. LeBeau
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License 
+# a long with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from useTEM import pluginManagement as plugm
 import logging
 import os
